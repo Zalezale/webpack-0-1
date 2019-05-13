a09b04512e544ab2b00eb06848ae41367bdbc014
郑春在 <zhengchunzai@zhengchunzaideMacBook-Pro.local>
2019-05-13 17:00:13 +0800

    添加文字，excel，json等文件的解析

--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,12 @@ module.exports = {
                         },
                     },
                 ],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     }
