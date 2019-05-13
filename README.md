61883df567d00677d4a3a2f1dfd5d470b4772d33
郑春在 <zhengchunzai@zhengchunzaideMacBook-Pro.local>
2019-05-13 19:21:38 +0800

    管理输出

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 #webpack4 默认会将与package.json同级的src文件下的index.js文件打包到同级的dist文件下
 二、管理资源
 #提取css，图片压缩，转换成base64，加载ttf、excel、json等文件
+三、管理输出
+#将入口文件直接打包到新生成的html中，并清理原来的dist文件夹中的文件
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './style.less'
+import add from './print'
 function component() {
     let element = document.createElement('div');
-
+    add()
     // lodash（目前通过一个 script 引入）对于执行这一行是必需的
     element.innerHTML = '大爬虫';
 
